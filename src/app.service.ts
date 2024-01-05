c9b20f574ce33093682f62976811be2567ceb0bb
Maxim <glockerswork@gmail.com>
2024-01-05 17:11:03 +0300

    add: add vscode config debug for myself

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,12 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    let a;
+    a = 5;
+    a = 6;
+    const b = 1;
+    const c = 3;
+    console.log(a, b, c);
     return 'Hello World!';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const t = 1;
+  console.log(t);
   await app.listen(3000);
 }
 bootstrap();
